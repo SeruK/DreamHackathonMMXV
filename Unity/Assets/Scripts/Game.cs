164b88d8054d1c91792d877ff6ddb9c0cacd8f9e
SebastianStrand <Sebastian@ImageForm.se>
2015-09-25 21:06:27 +0200

    A stupid old timer and other junk

---
+++ Unity/Assets/Scripts/Game.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+	protected void OnEnable() {
+		StartTimer( 1.0f, ( float dt ) => { Debug.Log( "dt: " + dt ); } ); 
+	}
+
+	protected void Update() {
+
+	}
+
+	protected void UpdateInput() {
+
+	}
+
+	// Timers
+
+	private void StartTimer( float duration, Action<float> tick ) {
+		StartTimer( duration, tick, null );
+	}
+	
+	private void StartTimer( float duration, Action<float> tick, Action done ) {
+		StartCoroutine( DoTimer( duration, tick, done ) );
+	}
+	
+	private IEnumerator DoTimer( float duration, Action<float> tick, Action done ) {
+		float t = Time.time;
+		float elapsed = 0.0f;
+		tick( 0.0f );
+		while( elapsed < duration ) {
+			yield return null;
+			float dt = Time.time - t;
+			elapsed += dt;
+			tick( Mathf.Min( elapsed / duration, 1.0f ) );
+		}
+		tick( 1.0f );
+		if( done != null ) {
+			done();
+		}
+	}
+}
